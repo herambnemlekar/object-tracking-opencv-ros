246a828e9e6f45bc005e5cfe228e844ac752142c
hsnemlekar <31831812+hsnemlekar@users.noreply.github.com>
2017-11-20 16:15:33 -0500

    Update OtpGenerator.py

--- OtpGenerator.py
+++ OtpGenerator.py
@@ -1,4 +1,7 @@
-import roslib, rospy
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('otp')
+import rospy
 import sys, time, os
 import numpy as np
 from geometry_msgs.msg import PointStamped, Point
@@ -8,8 +11,8 @@ class OtpGenerator:
 
     def __init__(self):
 
-        self.goal_pub = rospy.Publisher("/goal_position", Point)
-        self.time_pub = rospy.Publisher("/goal_time", Time)
+        self.goal_pub = rospy.Publisher("/OtpGenerator/goal_position", Point, queue_size=1)
+        self.time_pub = rospy.Publisher("/OtpGenerator/goal_time", Time, queue_size=1)
 
         self.D = []
         self.offset = 0.1
@@ -17,9 +20,13 @@ class OtpGenerator:
         self.baxter = []
         self.w = 1
 
-        self.data_sub = rospy.Subscriber("/skeleton_data", PointStamped, self.data, queue_size = 1)
+        self.data_sub = rospy.Subscriber("pyclient/skeleton_data", PointStamped, self.static, queue_size = 1)
 
-        self.D = np.append(self.D,self.data[1,1:4])
+    def static(self,data):
+	
+	self.data = data
+	
+	self.D = np.append(self.D,self.data[1,1:4])
         self.D = self.D[(self.D.shape(0) - 2):(self.D.shape(0) - 1),:]
 
         self.P_obj = np.add(self.data[1, 1:3], [0, self.offset, 0])
@@ -41,33 +48,36 @@ class OtpGenerator:
 
         self.otp_s = np.true_divide((P1 + P2 + P3 + P4), 4)
 
+	self.e_old = np.abs(og.P_old - og.otp_s)
+	self.e_new = np.abs(og.P_new - og.otp_s)
+
     def dynamic(self):
+	
+	if (self.e_new - self.e_old) < 0.02:
 
-        dt = self.D[1, 4] - self.D[2, 4]
+		dt = self.D[1, 4] - self.D[2, 4]
 
-        self.w = self.w - ((self.w*dt)/self.t_goal)
+		self.w = self.w - ((self.w*dt)/self.t_goal)
 
-        self.P_goal = (self.w*self.otp_s) + ((1-self.w)*self.P_obj)
+		self.P_goal = (self.w*self.otp_s) + ((1-self.w)*self.P_obj)
 
-        V = (self.P_new - self.P_old)/dt
+		V = (self.P_new - self.P_old)/dt
 
-        self.t_goal = (self.P_goal - self.P_obj)/V
+		self.t_goal = (self.P_goal - self.P_obj)/V
 
-        return self.t_goal, self.P_goal
+		#return self.t_goal, self.P_goal
+
+		self.goal_pub.publish(self.P_goal)
+		self.time_pub.publish(self.t_goal)
 
 
 def main(args):
     og = OtpGenerator()
-    e_old = np.abs(og.P_old - og.otp_s)
-    e_new = np.abs(og.P_new - og.otp_s)
-
-    if (e_new - e_old) < 0.02:
-        og.dynamic()
-        rospy.init_node('OtpGenerator', anonymous=True)
+    rospy.init_node('OtpGenerator', anonymous=True)
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        print "Shutting down"
+        print ("Shutting down")
 
 if __name__ == '__main__':
     main(sys.argv)
